79017d5cc999fb888dbc5c53c755958218821351
Zach Musgrave <musgrave.zm99@gmail.com>
2024-05-22 18:04:50 -0700

    add try catch to dependents controller and format responses

--- Api/Controllers/DependentsController.cs
+++ Api/Controllers/DependentsController.cs
@@ -1,6 +1,8 @@
 ﻿using Api.Dtos.Dependent;
 using Api.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using Swashbuckle.AspNetCore.Annotations;
 
 namespace Api.Controllers;
@@ -23,20 +25,82 @@ public class DependentsController : ControllerBase
     [HttpGet("{id}")]
     public async Task<ActionResult<ApiResponse<GetDependentDto>>> Get(int id)
     {
-        var dependent = await _context.Dependents.FindAsync(id);
-
-        if (dependent == null)
+        try
         {
-            return NotFound();
-        }
+            var dependent = await _context.Dependents.FindAsync(id);
 
-        return Ok(dependent);
+            if (dependent == null)
+            {
+                return NotFound(new ApiResponse<GetDependentDto>
+                {
+                    Success = false,
+                    Error = $"Dependent with ID {id} not found."
+                });
+            }
+
+            var dependentDto = new GetDependentDto
+            {
+                Id = dependent.Id,
+                FirstName = dependent.FirstName,
+                LastName = dependent.LastName,
+                DateOfBirth = dependent.DateOfBirth,
+                Relationship = dependent.Relationship
+            };
+
+            var result = new ApiResponse<GetDependentDto>
+            {
+                Data = dependentDto,
+                Success = true
+            };
+
+            return Ok(result);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, new ApiResponse<GetDependentDto>
+            {
+                Success = false,
+                //TODO come back to this spot and verify that this actually is useful
+                Error = ex.Message
+            });
+        }
+        
     }
 
     [SwaggerOperation(Summary = "Get all dependents")]
     [HttpGet("")]
     public async Task<ActionResult<ApiResponse<List<GetDependentDto>>>> GetAll()
     {
-        throw new NotImplementedException();
+        try
+        {
+            IQueryable<Dependent> dependentData = _context.Dependents;
+
+            var dependents = dependentData.Select(d => new GetDependentDto()
+            {
+                Id = d.Id,
+                FirstName = d.FirstName,
+                LastName = d.LastName,
+                DateOfBirth = d.DateOfBirth,
+                Relationship = d.Relationship
+            }).ToListAsync();
+            //TODO verify using await inside of this object cant be done in a better way
+            var result = new ApiResponse<List<GetDependentDto>>
+            {
+                Data = await dependents,
+                Success = true
+            };
+
+            return Ok(result);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, new ApiResponse<List<GetDependentDto>>
+            {
+                Success = false,
+                //TODO come back to this spot and verify that this actually is useful
+                Error = ex.Message
+            });
+        }
+
     }
 }
