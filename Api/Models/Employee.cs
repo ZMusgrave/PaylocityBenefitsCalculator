66fd971c09c042c70bb5e4ba33f6700bd5e13ac0
Zach Musgrave <musgrave.zm99@gmail.com>
2024-05-23 11:56:14 -0700

    add required attributes for expected data - validation for dependents

--- Api/Models/Employee.cs
+++ Api/Models/Employee.cs
@@ -1,11 +1,21 @@
-﻿namespace Api.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Api.Models;
 
 public class Employee
 {
+    [Required]
     public int Id { get; set; }
+    [Required]
     public string? FirstName { get; set; }
+    [Required]
     public string? LastName { get; set; }
+    [Required]
     public decimal Salary { get; set; }
+    [Required]
     public DateTime DateOfBirth { get; set; }
+    
+    [ValidateDependents]
     public ICollection<Dependent> Dependents { get; set; } = new List<Dependent>();
+  
 }
